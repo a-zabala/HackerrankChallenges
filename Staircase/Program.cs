fda1111b3beacab5f9d692e2e8e8b1a541f17c10
Angelina Zabala <angekr@aol.com>
2020-07-27 23:48:47 -0500

    refined Staircase

--- Staircase/Program.cs
+++ Staircase/Program.cs
@@ -6,53 +6,28 @@ namespace Staircase
     {
         static void Main(string[] args)
         {
-            //Still Working on this
+            //Input the size of the staircase the user wants
             Console.WriteLine("What size would you like the staircase (btn 0-100)");
             int n = Convert.ToInt32(Console.ReadLine());
 
             staircase(n);
-            Console.WriteLine("skip");
-            staircase2(n);
             Console.ReadLine();
 
-            // Complete the staircase function below.
-            static void staircase(int n)
-            {
-                string line = "";
-
-                string staircase = "";
-
-
-                for (int i = 0; i < n; i++)
-                {
-
-                    //Console.Write(" ");
-                    line = line + " ";
-
-                }
-                Console.WriteLine(line + "#");
-            }
-                static void staircase2(int n)
+            // Prints the staircase of the inputted size
+       
+                static void staircase(int n)
                 {
                     char hash = '#';
                     string blank = "";
-                    //string line = "";
 
-
-                    for (int i = n; i > 0; i--)
+                    for (int i = 0; i < n; i++)
                     {
-
-                        //Console.Write(" ", );
-                        string spaces = blank.PadLeft(i-1);
-                    string hashes = blank.PadLeft(n-i+1, hash);
+                        string spaces = blank.PadLeft(n-i-1);
+                        string hashes = blank.PadLeft(i+1, hash);
                         Console.WriteLine(spaces + hashes);
                     }
-                    //Console.WriteLine(line + "#");
-                    
 
-
-
-                }
+            }
 
         }
     }
