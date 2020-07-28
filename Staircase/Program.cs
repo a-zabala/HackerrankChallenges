c31ca1d2f1f5c84377ed54cf71e61c7a1a9720a0
Angelina Zabala <angekr@aol.com>
2020-07-27 23:37:15 -0500

    finished staircase

--- Staircase/Program.cs
+++ Staircase/Program.cs
@@ -11,6 +11,8 @@ namespace Staircase
             int n = Convert.ToInt32(Console.ReadLine());
 
             staircase(n);
+            Console.WriteLine("skip");
+            staircase2(n);
             Console.ReadLine();
 
             // Complete the staircase function below.
@@ -29,11 +31,28 @@ namespace Staircase
 
                 }
                 Console.WriteLine(line + "#");
-
-
-
-
             }
+                static void staircase2(int n)
+                {
+                    char hash = '#';
+                    string blank = "";
+                    //string line = "";
+
+
+                    for (int i = n; i > 0; i--)
+                    {
+
+                        //Console.Write(" ", );
+                        string spaces = blank.PadLeft(i-1);
+                    string hashes = blank.PadLeft(n-i+1, hash);
+                        Console.WriteLine(spaces + hashes);
+                    }
+                    //Console.WriteLine(line + "#");
+                    
+
+
+
+                }
 
         }
     }
